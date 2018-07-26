cdf27c2f1b00d5186384de44610ac82ff8f1b7ad
wei.dong <wei.dong@ectrip.com>
2018-07-26 12:39:14 +0800

    vue-router & vuex support

--- src/index.js
+++ src/index.js
@@ -2,15 +2,48 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Vuex from 'vuex';
 import App from './App.vue';
+import Routers from './routers.js';
 
 Vue.use(VueRouter);
 Vue.use(Vuex);
+
+const RouterConfig = {
+  mode:history,
+  routers: Routers
+}
+
+const router = new VueRouter(RouterConfig);
+
+router.beforeEach((to,from,next) =>{
+  window.document.title = to.meta.title;
+  next();
+});
+
+router.afterEach((to,from ,next)=>{
+  window.scrollTo(0,0);
+});
+const store = new Vuex.Store({
+  state: {
+
+  },
+  mutations: {
+
+  },
+  getters: {
+
+  },
+  actions: {
+
+  }
+})
 
 const root = document.createElement("div");
 document.body.appendChild(root);
 
 new Vue({
-  render:h => {
-      return h(App);
+  store:store,
+  router:router,
+  render: h => {
+    return h(App);
   }
 }).$mount(root);

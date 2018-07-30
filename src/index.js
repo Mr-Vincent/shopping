33d8fedef7c8da3e6feac889b3a5cab4664ac4a7
wei.dong <wei.dong@ectrip.com>
2018-07-30 13:43:28 +0800

    organize code

--- src/index.js
+++ src/index.js
@@ -1,49 +1,9 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Vuex from 'vuex';
-import App from './App.vue';
-import Routers from './routers.js';
 
-Vue.use(VueRouter);
-Vue.use(Vuex);
-
-const RouterConfig = {
-  mode:history,
-  routers: Routers
-}
-
-const router = new VueRouter(RouterConfig);
-
-router.beforeEach((to,from,next) =>{
-  window.document.title = to.meta.title;
-  next();
-});
-
-router.afterEach((to,from ,next)=>{
-  window.scrollTo(0,0);
-});
-const store = new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  getters: {
-
-  },
-  actions: {
-
-  }
-})
-
-const root = document.createElement("div");
-document.body.appendChild(root);
+import  Vue from  'vue'
+import App from './App.vue'
 
 new Vue({
-  store:store,
-  router:router,
-  render: h => {
-    return h(App);
-  }
-}).$mount(root);
+    el: '#app',
+    components: {App},
+    template: '<App/>'
+})
